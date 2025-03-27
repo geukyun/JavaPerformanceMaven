ad429c0b49740e3392114801c180506f62f6edaf
bbabang09 <gegulee132@naver.com>
2025-03-28 03:12:09 +0900

    test

---
+++ src/com/perf/string/StringBufferTest1.java
@@ -0,0 +1,18 @@
+package src.com.perf.string;
+
+public class StringBufferTest1 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("ABCD");
+		StringBufferTest1 sbt = new StringBufferTest1();
+		sbt.check(sb);
+
+	}
+	
+	public void check(CharSequence cs) {
+		StringBuffer sb = new StringBuffer(cs);
+		System.out.println("sb.length = "+sb.length());
+	}
+
+}
